6f0f6dd5e1b4ce664e13309c8c3386e725c6903b
KMalik <Kazi.Malik@surbanajurong.com>
2023-06-22 16:00:33 +0600

    Best Time to Buy and Sell Stock Solution

--- Program.cs
+++ Program.cs
@@ -5,11 +5,16 @@
         static void Main(string[] args)
         {
             //Two Sum Testing
-            int[] numbers = { 2, 7, 11, 15 };
-            int target = 9;
-            int[] result = twoSum.TwoSum(numbers, target);
+            //int[] numbers = { 2, 7, 11, 15 };
+            //int target = 9;
+            //int[] result = twoSum.TwoSum(numbers, target);
 
-            Console.WriteLine(string.Join(" ", result));
+            //Console.WriteLine(string.Join(" ", result));
+
+            //Best Time to Buy and Sell Stock
+
+            int[] prices = { 7, 1, 5, 3, 6, 4 };
+            Console.WriteLine(bestTimeToBuyAndSellStock.MaxProfit(prices));
         }
     }
 }
---
+++ bestTimeToBuyAndSellStock.cs
@@ -0,0 +1,20 @@
+﻿namespace LetUsLeetCode
+{
+    public static class bestTimeToBuyAndSellStock
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+
+            foreach (int price in prices)
+            {
+                minPrice = Math.Min(price, minPrice);
+
+                maxProfit = Math.Max(maxProfit, price - minPrice);
+            }
+
+            return maxProfit;
+        }
+    }
+}
